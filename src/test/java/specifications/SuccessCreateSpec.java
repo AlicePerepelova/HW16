8032e5bd6aa62766dc4b732567d84417161fb40f
Alisa Perepelova <alice-lilo@mail.ru>
2025-02-06 09:56:34 +0300

    2 errors

--- src/test/java/specifications/SuccessCreateSpec.java
+++ src/test/java/specifications/SuccessCreateSpec.java
@@ -7,7 +7,7 @@ import io.restassured.specification.ResponseSpecification;
 import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.filter.log.LogDetail.BODY;
 import static io.restassured.filter.log.LogDetail.STATUS;
-import static io.restassured.filter.log.RequestLoggingFilter.with;
+import static io.restassured.RestAssured.with;
 import static io.restassured.http.ContentType.JSON;
 
 public class SuccessCreateSpec {
